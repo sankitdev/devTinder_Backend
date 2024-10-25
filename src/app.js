e5f1f2301550e2186f5e780e08a0083ebe5def80
Ankit Singh <ankitsingh635101@gmail.com>
2024-10-25 09:44:37 +0530

    protected route practice

--- src/app.js
+++ src/app.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const connectDB = require("../src/config/database");
 const app = express();
-const User = require("./models/user");
+const UserModel = require("./models/user");
+const { authAdmin } = require("./middlewares/auth");
 // Make sure to connect to database first after then start server.
 app.use(express.json());
+app.use("/admin", authAdmin);
+app.get("/admin/getAllData", (req, res, next) => {
+  res.send("You have access to All Data");
+});
+app.get("/admin/showData", (req, res, next) => {
+  res.send("You have access to showData");
+});
 app.post("/signup", async (req, res) => {
-  const userData = User(req.body);
+  const userData = UserModel(req.body);
   try {
     res.send("User is registered");
     await userData.save();
+  } catch (error) {
+    console.error("Error", error);
+  }
+});
+
+app.post("/user", async (req, res) => {
+  const users = await UserModel.findOne(req.body);
+  console.log(users);
+  res.send(users);
+});
+app.get("/feed", async (req, res) => {
+  const users = await UserModel.find({});
+  try {
+    if (!users) {
+      res.status(404).send("No user found");
+    } else {
+      res.send(users);
+    }
   } catch (error) {
     console.error("Error", error);
   }
