16dbd01b33723b357cfd2eaabce972b9c2b69b23
Ankit Singh <ankitsingh635101@gmail.com>
2024-10-27 12:44:00 +0530

    Add data sanitization on Schema level using default validate function and validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,62 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 3,
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxLength: 40,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password not strong");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Please Enter Correct Gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Give proper URL");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 3,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
