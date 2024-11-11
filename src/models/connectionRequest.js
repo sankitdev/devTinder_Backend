94c28b30fa3bc73be55a1cc6a31614e0b1f69d89
Ankit Singh <ankitsingh635101@gmail.com>
2024-11-11 16:13:27 +0530

    ConnectionRequest Api Handler

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: "{VALUE} is not supported",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const ConnectionRequestModel = mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+module.exports = ConnectionRequestModel;
