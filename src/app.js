3f07e4e3d034858de31e5a32b802d6a085fc4cea
Ankit Singh <ankitsingh635101@gmail.com>
2024-11-11 16:13:04 +0530

    Routes Update

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ const connectDB = require("../src/config/database");
 const authRouter = require("../src/routers/authRouter");
 const profileRouter = require("../src/routers/profileRouter");
 const userRouter = require("./routers/userRouter");
+const requestRouter = require("./routers/requestRouter");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/", authRouter);
-app.use("/profile", profileRouter);
-app.use("/user", userRouter);
+app.use("/", profileRouter);
+app.use("/", userRouter);
+app.use("/", requestRouter);
 
 connectDB().then(() => {
   console.log("DB Successfully connected");
