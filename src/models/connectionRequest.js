3b9ea4e16f977347d1a2cd741b5ca5a684233abf
Ankit Singh <ankitsingh635101@gmail.com>
2024-11-12 08:36:02 +0530

    Remove the unique flag from fromUserId

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -6,7 +6,6 @@ const connectionRequestSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      unique: true,
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
