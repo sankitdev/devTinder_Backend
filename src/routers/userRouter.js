bcba0e40c8f69607ebb8aae5cc654dfc621c15c4
Ankit Singh <ankitsingh635101@gmail.com>
2024-11-12 08:37:51 +0530

    Design sending Request & Reviewing API

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,50 @@
 const express = require("express");
 const userRouter = express.Router();
+const UserModel = require("../models/user");
+const ConnectionRequestModel = require("../models/connectionRequest");
+const { authUser } = require("../middlewares/auth");
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "gender",
+  "skills",
+  "about",
+  "photoUrl",
+];
+userRouter.get("/user/requests", authUser, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //Banda login h ya nhi - Done
+    const data = await ConnectionRequestModel.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", USER_SAFE_DATA);
+    if (!data) {
+      return res.status(404).json({ message: "No request found" });
+    }
+    res.json({ message: data });
+  } catch (error) {
+    res.status(400).send("Error:", error.message);
+  }
+});
+userRouter.get("/user/connections", authUser, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const data = await ConnectionRequestModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
 
+    if (!data) {
+      return res.status(404).json({ message: "No request found" });
+    }
+    res.json({ message: data });
+  } catch (error) {
+    res.status(400).send("Error:", error.message);
+  }
+});
 userRouter.get("/feed", async (req, res) => {
   const users = await UserModel.find({});
   try {
