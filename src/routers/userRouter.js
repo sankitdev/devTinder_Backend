6a5eeb48a94eadab31a5af3109b7992c4e88bc87
Ankit Singh <ankitsingh635101@gmail.com>
2024-11-19 13:06:48 +0530

    Create feed Api

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -11,18 +11,19 @@ const USER_SAFE_DATA = [
   "about",
   "photoUrl",
 ];
-userRouter.get("/user/requests", authUser, async (req, res) => {
+userRouter.get("/user/request/pending", authUser, async (req, res) => {
   try {
     const loggedInUser = req.user;
-    //Banda login h ya nhi - Done
     const data = await ConnectionRequestModel.find({
       toUserId: loggedInUser._id,
       status: "interested",
     }).populate("fromUserId", USER_SAFE_DATA);
+
+    const filteredData = data.map((res) => res.fromUserId);
     if (!data) {
       return res.status(404).json({ message: "No request found" });
     }
-    res.json({ message: data });
+    res.json({ message: filteredData });
   } catch (error) {
     res.status(400).send("Error:", error.message);
   }
@@ -40,21 +41,40 @@ userRouter.get("/user/connections", authUser, async (req, res) => {
     if (!data) {
       return res.status(404).json({ message: "No request found" });
     }
-    res.json({ message: data });
+    const filteredData = data.map((res) => res.fromUserId);
+    res.json({ message: filteredData });
   } catch (error) {
     res.status(400).send("Error:", error.message);
   }
 });
-userRouter.get("/feed", async (req, res) => {
-  const users = await UserModel.find({});
+userRouter.get("/feed", authUser, async (req, res) => {
+  // login in user should not see people with the below conditions:-
+  // 1. Accepted Request or reject request
+  // 2. Sent Request
   try {
-    if (!users) {
-      res.status(404).send("No user found");
-    } else {
-      res.send(users);
-    }
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 20 ? 20 : limit;
+    const skip = (page - 1) * 10;
+    const connectionData = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    const hideUserFromFeed = new Set();
+    connectionData.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+    const feedUser = await UserModel.find({
+      _id: { $nin: Array.from(hideUserFromFeed) },
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+    console.log(hideUserFromFeed);
+    res.send(feedUser);
   } catch (error) {
-    console.error("Error", error);
+    res.status(404).json({ message: "Something is wrong " + error.message });
   }
 });
 
